e200159559f13341c56a692a3eb87e0dd0f59658
AlexTicovschi <alexmticovschi@gmail.com>
2018-08-31 12:16:15 +0100

    getting and displaying currency options from CoinMarketCap API

--- js/cryptoAPI.js
+++ js/cryptoAPI.js
@@ -1,3 +1,18 @@
 class CryptoAPI {
-    
+
+    // Get all the cryptocurrencies
+    async getCryptoCurrenciesList() {
+        // Make a GET request to https://api.coinmarketcap.com/v1/ticker/ and wait for the request to finish
+        // When the request is done, pass the resolved value to the url variable.
+        //const url = await fetch('https://api.coinmarketcap.com/v1/ticker/');
+        const url = await fetch('https://api.coinmarketcap.com/v2/listings/');
+
+        // get the JSON version of the response and then pass the result to the cryptoCurrencies variable
+        const cryptoCurrencies = await url.json();
+
+        // return the object
+        return {
+            cryptoCurrencies
+        }
+    }
 }
--- js/ui.js
+++ js/ui.js
@@ -1,3 +1,26 @@
 class UI {
+    constructor() {
+        this.init();
+    }
+    init() {
+        this.displayCryptoCurrencies();
+    }
+    // Display the <option> for the form
+    displayCryptoCurrencies() {
+        cryptoAPI.getCryptoCurrenciesList()
+            .then(res => {
+                const cryptoCurrencies = res.cryptoCurrencies.data;
+                
+                // Build the <select> from the REST API
+                const select = document.getElementById('cryptocurrency');
 
+                cryptoCurrencies.forEach(currency => {
+                    // everytime we loop through the array add a new option
+                    const option = document.createElement('option');
+                    option.value = currency.id;
+                    option.appendChild(document.createTextNode(currency.name));
+                    select.appendChild(option)
+                })
+            })
+    }
 }
